aedc9b5c843cc64ec63db2de8d2186523997a8ae
Dominick Hera <dominickhera@me.com>
2018-10-05 20:05:00 -0400

    fixed

--- src/dataGrab.js
+++ src/dataGrab.js
@@ -7,7 +7,7 @@ export function getCryptoRankings() {
             console.log(response.data.data);
             var cryptoData = {};
             var cryptoInfo = [];
-            let listLength = response.data.length;
+            let listLength = response.data.data.length;
             console.log("length of list is " + listLength);
             // var shitWorkAround = [1,2,52,328,512,825,1027,1765,1831,2010];
             cryptoData.cryptoInfo = cryptoInfo;
@@ -63,8 +63,8 @@ export function getCryptoRankings() {
  
 export function SearchExtension(query) {
         return fetch(`https://api.coinmarketcap.com/v2/ticker/${query}/?structure=array`)
-            .then(resp => resp.items.data.map(item => ({
-                title: item.data.name,
-                symbol: item.data.symbol,
+            .then(resp => resp.items.data.data.map(item => ({
+                title: item.data.data.name,
+                symbol: item.data.data.symbol,
             })));   
 }
