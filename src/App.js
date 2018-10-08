ed185e2bf8d65e6c3eb1bd66a40ba8bd7aa267e3
Dominick Hera <dominickhera@me.com>
2018-10-08 15:30:19 -0400

    hmmmm

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ReactTable from "react-table";
 import "react-table/react-table.css";
 const request = require('axios');
 
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -33,61 +34,58 @@ class App extends Component {
       });
     }
 
-    getCryptoData() { 
-      var promise = new Promise((resolve, reject) => { 
-        setTimeout(() => {
-        request
-        .get('https://api.coinmarketcap.com/v2/ticker/?sort=rank&structure=array', {crossdomain: true})
-        .then(function (response) {
-            // console.log(response.data.data);
-            var cryptoData = {};
-            var cryptoInfo = [];
-            let listLength = response.data.data.length;
+    getCryptoData() {
+      request
+      .get('https://api.coinmarketcap.com/v2/ticker/?sort=rank&structure=array', {crossdomain: true})
+      .then(function (response) {
+          // console.log(response.data.data);
+          var cryptoData = {};
+          var cryptoInfo = [];
+          let listLength = response.data.data.length;
 
-            cryptoData.cryptoInfo = cryptoInfo;
-            var i;
-            for(i = 0; i < listLength; i++) {
-                var name = response.data.data[i].name;
-                var symbol = response.data.data[i].symbol;
-                var rank = response.data.data[i].rank;
-                var price = response.data.data[i].quotes.USD.price;
-                var percent_change_1hr = response.data.data[i].quotes.USD.percent_change_1h;
-                var percent_change_7d = response.data.data[i].quotes.USD.percent_change_7d;
-                var percent_change_24hr = response.data.data[i].quotes.USD.percent_change_24h;
-                
-                var detailedCryptoInfo = {
-                    "name": name,
-                    "symbol": symbol,
-                    "rank": rank,
-                    "price": price,
-                    "percent_change_1h": percent_change_1hr,
-                    "percent_change_7d": percent_change_7d,
-                    "percent_change_24h": percent_change_24hr
-                }
-                cryptoData.cryptoInfo.push(detailedCryptoInfo);
-            }
-            console.log("crypto data is " + cryptoData);
-            // return cryptoData;
-            resolve(cryptoData)
-            localStorage.setItem("cryptoData", JSON.stringify(cryptoData));
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-        }, 3000);
-    });
-    return promise;
-  
-  }
+          cryptoData.cryptoInfo = cryptoInfo;
+          var i;
+          for(i = 0; i < listLength; i++) {
+              var name = response.data.data[i].name;
+              var symbol = response.data.data[i].symbol;
+              var rank = response.data.data[i].rank;
+              var price = response.data.data[i].quotes.USD.price;
+              var percent_change_1hr = response.data.data[i].quotes.USD.percent_change_1h;
+              var percent_change_7d = response.data.data[i].quotes.USD.percent_change_7d;
+              var percent_change_24hr = response.data.data[i].quotes.USD.percent_change_24h;
+              
+              var detailedCryptoInfo = {
+                  "name": name,
+                  "symbol": symbol,
+                  "rank": rank,
+                  "price": price,
+                  "percent_change_1h": percent_change_1hr,
+                  "percent_change_7d": percent_change_7d,
+                  "percent_change_24h": percent_change_24hr
+              }
+              cryptoData.cryptoInfo.push(detailedCryptoInfo);
+          }
+          console.log("crypto data is " + cryptoData);
+          // return cryptoData;
+          this.setState({
+            data: cryptoData.cryptoInfo
+          })
+          localStorage.setItem("cryptoData", JSON.stringify(cryptoData));
+      })
+      .catch(function (error) {
+          console.log(error);
+      })
+}
 
     componentDidMount() {
-      getCryptoRankings()
-      .then((data) => {
-        // console.log('This happens 7th.');
-        this.setState({
-          data: data.cryptoInfo
-        });
-      });
+      this.getCryptoData();
+      // this.getCryptoRankings()
+      // .then((data) => {
+      //   // console.log('This happens 7th.');
+      //   this.setState({
+      //     data: data.cryptoInfo
+      //   });
+      // });
     }
 
     render() {
